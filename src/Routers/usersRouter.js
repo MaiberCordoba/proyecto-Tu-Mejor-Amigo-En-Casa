6a0885765555f4cf8bfdb69d7b4e465cb5445147
maiberCordoba <cordobamaiber@gmail.com>
2025-05-03 15:54:51 -0500

    creando endpoint para logearse

--- src/Routers/usersRouter.js
+++ src/Routers/usersRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { getUsersMJC, PostUsersMJC } from "../Controllers/usersController.js";
+import { getUsersMJC, LoginMJC, PostUsersMJC } from "../Controllers/usersController.js";
 
 export const rutaUser = Router();
     rutaUser.get("/users",getUsersMJC)
     rutaUser.post("/users",PostUsersMJC)
+    rutaUser.post("/login",LoginMJC)
