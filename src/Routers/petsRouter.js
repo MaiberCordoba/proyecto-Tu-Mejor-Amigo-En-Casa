839f45ffecefd24274066eb8202bbb3a5b5d2361
maiberCordoba <cordobamaiber@gmail.com>
2025-05-07 14:52:53 -0500

    agregando el metodo upload a los endpoints de crear y editar mascotas que multer espere un archivo

--- src/Routers/petsRouter.js
+++ src/Routers/petsRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { deletePetsMJC, getIdPetsMJC, listPetsMJC, patchPetsMJC, PostPetsMJC } from "../Controllers/petsControllerMJC.js";
+import { upload } from "../middleware/upload.js";
 
 export const route = Router();
 
 route.get("/pets",listPetsMJC);
-route.post("/pets",PostPetsMJC);
-route.patch("/pets/:id",patchPetsMJC);
+route.post('/pets', upload.single('photo'),PostPetsMJC);
+route.patch("/pets/:id", upload.single('photo'), patchPetsMJC);
 route.delete("/pets/:id", deletePetsMJC);
 route.get("/pets/:id", getIdPetsMJC);
