9bb7c53768b419b423bfd99af501d8b3f665120d
maiberCordoba <cordobamaiber@gmail.com>
2025-05-12 04:42:18 -0500

    probando rutas y host que corre backend

--- src/middleware/uploadMJC.js
+++ src/middleware/uploadMJC.js
@@ -4,12 +4,12 @@ import path from "path";
 // Definimos el almacenamiento
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join("public", "pets-photos")); // Carpeta donde se guardan
+    cb(null, path.join("public", "pets-photos")); 
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname); // Extrae la extensión (.jpg, .png)
-    cb(null, uniqueSuffix + ext); // Ej: 1746378662959-424814978.png
+    const ext = path.extname(file.originalname); 
+    cb(null, uniqueSuffix + ext); 
   },
 });
 
