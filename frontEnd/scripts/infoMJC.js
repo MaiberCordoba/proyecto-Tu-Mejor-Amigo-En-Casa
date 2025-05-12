d307dd77e3c8222b671c9db1d0d0155fb3a04531
maiberCordoba <cordobamaiber@gmail.com>
2025-05-11 22:14:31 -0500

    script para listar la informacion de la mascota elejida

--- frontEnd/scripts/infoMJC.js
+++ frontEnd/scripts/infoMJC.js
@@ -1,6 +1,42 @@
 const urlParamsMJC = new URLSearchParams(window.location.search);
-const petId = urlParams.get('id');
+const petId = urlParamsMJC.get('id');
 
-window.backList = () => {
-    window.location.href = '/frontEnd/pages/list.html'
+
+const photoImg = document.getElementById('photo');
+const nameSpan = document.getElementById('name');
+const raceSpan = document.getElementById('race');
+const categorySpan = document.getElementById('category');
+const genderSpan = document.getElementById('gender');
+const estadoSpan = document.getElementById('estado');
+
+async function infoMJC() {
+    try {
+        const response = await fetch(`${API_URL}/api/pets/${petId}`);
+        const mascota = await response.json();
+
+        console.log(mascota);
+
+        // Rellena los spans con los datos recibidos
+        nameSpan.textContent = mascota.name_PetsMJC || 'No disponible';
+        raceSpan.textContent = mascota.races.name_RacesMJC || 'No disponible';
+        categorySpan.textContent = mascota.categories.name_CategoriesMJC || 'No disponible';
+        genderSpan.textContent = mascota.Genders.name_GendersMJC || 'No disponible';
+        estadoSpan.textContent = mascota.estadoMJC || 'No disponible';
+
+        //  img pets
+        if (mascota.photoMJC) {
+            photoImg.src = `${API_URL}${mascota.photoMJC}`
+        }
+
+    } catch (error) {
+        console.error('Error al cargar información de la mascota:', error);
+    }
 }
+
+
+infoMJC();
+
+// Botón para regresar a la lista
+window.backList = () => {
+    window.location.href = '/frontEnd/pages/list.html';
+};
