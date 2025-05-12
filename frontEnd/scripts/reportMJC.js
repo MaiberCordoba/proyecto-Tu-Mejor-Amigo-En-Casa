f52a2a0aab2dc0e3a712c47dc1588cd692053bee
maiberCordoba <cordobamaiber@gmail.com>
2025-05-12 01:52:56 -0500

    realizando css  html y script de reportes

--- frontEnd/scripts/reportMJC.js
+++ frontEnd/scripts/reportMJC.js
@@ -1,3 +1,49 @@
+async function cargarReportes() {
+    // Reporte 1
+    const res1 = await fetch(`${API_URL}/api/report/1`);
+    const data1 = await res1.json();
+    console.log(data1.conteo_mascotas_adoptadas)
+    const tbody1 = document.querySelector('#tabla-reporte-1 tbody');
+data1.conteo_mascotas_adoptadas.forEach(item => {
+  const fila = `<tr>
+    <td>${item.categoria}</td>
+    <td>${item.genero}</td>
+    <td>${item.raza}</td>
+    <td>${item.cantidad}</td>
+  </tr>`;
+  tbody1.insertAdjacentHTML('beforeend', fila);
+});
+    // Reporte 2
+    const res2 = await fetch(`${API_URL}/api/report/2`);
+    const data2 = await res2.json();
+  
+    const tbody2 = document.querySelector('#tabla-reporte-2 tbody');
+    data2.conteo_por_estado_y_categoria.forEach(item => {
+      const fila = `<tr>
+                      <td>${item.categoria}</td>
+                      <td>${item.estado}</td>
+                      <td>${item.cantidad}</td>
+                    </tr>`;
+      tbody2.insertAdjacentHTML('beforeend', fila);
+    });
+  }
+  
+  function descargarPDF() {
+    const elemento = document.getElementById("reporte-container");
+    html2pdf()
+      .from(elemento)
+      .set({
+        margin: 0.5,
+        filename: 'reporte_mascotas.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+      })
+      .save();
+  }
+  
+  document.addEventListener('DOMContentLoaded', cargarReportes);
+
 window.backList = () => {
     window.location.href = '/frontEnd/pages/list.html';
 };
